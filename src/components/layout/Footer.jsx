35e517f40a84ef9703f0b3f909a3c4bea6458b1b
KyleDev <kyledev10282001@gmail.com>
2026-01-10 18:31:34 +0800

    Done:

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
                     <div>
-                        <h3 className='text-2xl font-semibold mb-4'>{personal_info.name}</h3>
+                        <h3 className='text-2xl font-semibold mb-4'></h3>
                     </div>
                 </div>
             </div>
