363157eed1d19a7dba249468e7b80aee106a3195
KyleDev <kyledev10282001@gmail.com>
2026-01-10 10:46:42 +0800

    Done:

--- src/data/skills.js
+++ src/data/skills.js
@@ -5,98 +5,98 @@ export const skills = [
         name: 'JavaScript',
         icon: 'Code',
         level: 'Advanced',
-        experience: '5 years'
+        experience: '3 years'
     },
     {   
         id: 2,
         name: 'React.js',
         icon: 'Layers',
         level: 'Advanced',
-        experience: '4 years'
+        experience: '1.5 years'
     },
     {
         id: 3,
         name: 'Node.js',    
         icon: 'Server',
         level: 'Intermediate',
-        experience: '3 years'
+        experience: '1.5 years'
     },
     {
         id: 4,
         name: 'CSS',
         icon: 'Palette',
         level: 'Advanced',
-        experience: '5 years'
+        experience: '3 years'
     },
     {
         id: 5,
         name: 'Python',
         icon: 'Code2',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1 years'
     },
     {
         id: 6,
         name: 'Next.js',
         icon: 'Zap',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1 years'
     },
     {
         id: 7,
         name: 'Tailwind CSS',
         icon: 'Sparkles',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '3 years'
     },
     {
         id: 8,
         name: 'TypeScript',
         icon: 'FileCode',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1.5 years'
     },
     {
         id: 9,
         name: 'HTML5',
         icon: 'FileCode',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '3 years'
     },
     {
         id: 10,
         name: 'Bootstrap',
         icon: 'Grid',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '3 years'
     },
     {
         id: 11,
         name: 'Redux',
         icon: 'Repeat',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1.5 years'
     },
     {
         id: 12,
         name: 'Express.js',
         icon: 'Network',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1.5 years'
     },
     {
         id: 13,
         name: 'MongoDB',
         icon: 'Database',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1.5 years'
     },
     {
         id: 14,
         name: 'MySQL',
         icon: 'Database',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '3 years'
     },
     {
         id: 15,
@@ -110,7 +110,7 @@ export const skills = [
         name: '.NET',
         icon: 'Box',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1.5 years'
     },
     {
         id: 17,
@@ -145,14 +145,14 @@ export const skills = [
         name: 'C#',
         icon: 'Code',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1.5 years'
     },
     {
         id: 22,
         name: 'Inertia',
         icon: 'Zap',
         level: 'Intermediate',
-        experience: '2 years'
+        experience: '1.5 years'
     },
 
 ];
